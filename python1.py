814306de2f48736a100ac31f48ad3fa94e91327a
ericzhou571 <57415741+ericzhou571@users.noreply.github.com>
2020-04-09 13:48:28 +0200

    clean

--- python1.py
+++ python1.py
@@ -1,16 +1,2 @@
 print('hello world!')
 print('this is from folder 2')
-对城市的v草地上ds
-cdsdcsd @staticmethod
-def  d dcdc
-对差点
-
-(parameter_list):
-    pass
-
-单纯的对对的小葱
-
- 
-  cwcewd我行
-  行
-  形式sd dsedewcxy xy xy xy
